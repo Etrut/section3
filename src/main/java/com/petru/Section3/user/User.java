a09c621be096fdb4c7772a762453c272408392f9
Petru <etruttt@gmail.com>
2020-12-29 14:57:11 +0100

    primo

---
+++ src/main/java/com/petru/Section3/user/User.java
@@ -0,0 +1,51 @@
+package com.petru.Section3.user;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Date birthDate;
+    //getter e default Costructor OBBLIGATORI
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
